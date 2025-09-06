88e158e996c4fa8eb41d6c6b3c74591f73cc3646
David-best1 <tsaturov.david@yandex.ru>
2025-09-06 20:08:42 +0300

    7

--- Client/MCG_Client.cpp
+++ Client/MCG_Client.cpp
@@ -8,6 +8,9 @@
 
 const int PORT = 8080;
 
+using namespace std;
+
+// Функция приёма сообщений от сервера
 void receive_messages(SOCKET client_socket)
 {
     char buffer[1024];
@@ -15,12 +18,26 @@ void receive_messages(SOCKET client_socket)
     {
         memset(buffer, 0, sizeof(buffer));
         int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-        if (bytes_received > 0)
+        if (bytes_received <= 0)
         {
-            std::string message(buffer, bytes_received);
-            std::cout << message << std::endl;
+            break; // Сервер завершил работу или произошел разрыв
+        }
+        else
+        {
+            string message(buffer, bytes_received);
+            cout << message << endl;
         }
     }
+}
+
+bool is_connected(SOCKET sock)
+{
+    fd_set readfds;
+    timeval timeout = { 0 };
+    FD_ZERO(&readfds);
+    FD_SET(sock, &readfds);
+    select((int)(sock + 1), &readfds, nullptr, nullptr, &timeout);
+    return !FD_ISSET(sock, &readfds);
 }
 
 int main()
@@ -28,39 +45,62 @@ int main()
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    std::string ip_address;
-    std::cout << "Enter server IP address: ";
-    std::getline(std::cin, ip_address);
+    SOCKET client_socket = INVALID_SOCKET;
+    bool running = true;
 
-    SOCKET client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (client_socket == INVALID_SOCKET)
+    while (running)
     {
-        std::cerr << "Failed to create socket." << std::endl;
-        return 1;
-    }
+        string ip_address;
+        cout << "Enter server IP address: ";
+        getline(cin, ip_address);
 
-    sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, ip_address.c_str(), &server_addr.sin_addr);
+        // Создание сокета
+        client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (client_socket == INVALID_SOCKET)
+        {
+            cerr << "Failed to create socket." << endl;
+            continue;
+        }
 
-    if (connect(client_socket, (sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
-    {
-        std::cerr << "Failed to connect to server." << std::endl;
+        sockaddr_in server_addr;
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(PORT);
+        inet_pton(AF_INET, ip_address.c_str(), &server_addr.sin_addr);
+
+        // Подключение к серверу
+        if (connect(client_socket, (sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+        {
+            cerr << "Failed to connect to server." << endl;
+            closesocket(client_socket);
+            continue;
+        }
+
+        cout << "Connected to the server." << endl;
+
+        // Запуск потока для приёма сообщений
+        thread receive_thread(receive_messages, client_socket);
+
+        string message;
+        while (is_connected(client_socket))
+        {
+            getline(cin, message);
+            if (message == "disconnect")
+            {
+                cout << "Disconnecting from server." << endl;
+                break;
+            }
+            send(client_socket, message.c_str(), message.size() + 1, 0);
+        }
+
+        // Прерывание приема сообщений
+        receive_thread.detach(); // Отделяем поток, иначе join может заблокироваться
+
         closesocket(client_socket);
-        return 1;
+        client_socket = INVALID_SOCKET;
+
+        cout << "Disconnected from server. Enter new IP address or exit.\n";
     }
 
-    std::thread receive_thread(receive_messages, client_socket);
-
-    std::string message;
-    while (true)
-    {
-        std::getline(std::cin, message);
-        send(client_socket, message.c_str(), message.size() + 1, 0);
-    }
-
-    closesocket(client_socket);
     WSACleanup();
     return 0;
 }
